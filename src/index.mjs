551f7f244362cdd188c4f78c9d15ff7cbe6f0874
Hiroki Yamagishi <95039317+Hyzsa@users.noreply.github.com>
2023-10-15 22:32:14 +0000

    完了ボタンと削除ボタンを追加する処理を追加

--- src/index.mjs
+++ src/index.mjs
@@ -10,7 +10,14 @@ const onClickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  const deletedButton = document.createElement("button");
+  deletedButton.innerText = "削除";
+
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deletedButton);
 
   document.getElementById("incomplete-list").appendChild(div);
 };
